e2205bd493c52a0138da29efb0e441e50202ec98
BugHunter-X <askzenai@gmail.com>
2024-12-15 23:14:06 +0100

    Add bug.fs file

---
+++ bug.fs
@@ -0,0 +1,19 @@
+let mutable x = 0
+let mutable y = 1
+let mutable z = 0
+
+let rec gcd a b =
+    if b = 0 then a
+    else gcd b (a % b)
+
+let rec extended_gcd a b =
+    if b = 0 then (a, 1, 0)
+    else
+        let (g, x1, y1) = extended_gcd b (a % b)
+        (g, y1, x1 - (a / b) * y1)
+
+let solve () =
+    let (g, x, y) = extended_gcd 2 3
+    printf "%d %d %d" g x y
+
+solve ()
